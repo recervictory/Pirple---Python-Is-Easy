5a0961d0c3fe679762ea96ce3ad023f971350747
Victor Banerjee <recervictory@gmail.com>
2020-01-16 07:14:17 +0000

    Initial commit

---
+++ Homework Assignment #1: Variables.py
@@ -0,0 +1,73 @@
+"""
+Homework Assignment #1: Variables
+
+
+Details:
+ 
+What's your favorite song?
+
+Think of all the attributes that you could use to describe that song. That is: all of it's details or "meta-data". These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many different characteristics as you can.
+
+In your text editor, create an empty file and name it main.py
+
+Now, within that file, list all of the attributes of the song, one after another, by creating variables for each attribute, and giving each variable a value. Here's an example:
+
+```
+Genre = "Jazz"
+
+```
+
+Give each variable its own line. Then, after you have listed the variables, print each one of them out.
+
+ For example:
+
+```
+
+Artist = "Dave Brubeck"
+Genre = "Jazz"
+DurationInSeconds = 328
+
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+
+
+```
+Your actual assignment should be significantly longer than the example above. Think of as many characteristics of the song as you can. Try to use Strings,  Integers and Decimals (floats)!
+
+Extra Credit:
+
+By this far in the course you probably intuitively figured out how comments work. There are two types of comments:
+
+Single line comments: They start with a pound-sign, like so
+
+ # I am a single-line comment
+
+Multi-line comments: They begin and end with 3 double-quotes in a row, like so:
+
+"
+I am a
+Multi-line comment
+"
+
+For extra credit, add comments to your code, that explain the different attributes. Also add a title to the top of your file explaining what the file is, and what it's for.
+
+
+
+"""
+
+# Attribute of My Favorite Song
+SongName = "Kyunki Tum Hi Ho Ab Tum Hi Ho"
+Movie = "Aashiqui 2"
+Artist = "Arijit Singh"
+Genre = "Bolywood"
+DurationInSeconds = 300
+AlbumPrice = 200.50
+
+# Printing the Attributes
+print(SongName)
+print(Movie)
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+print(AlbumPrice)
